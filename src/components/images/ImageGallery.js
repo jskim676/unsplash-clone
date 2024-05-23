a2936877b903565142a2a391d490d70965099991
jskim676 <wkxndlsvk@gmail.com>
2024-05-23 23:28:15 +0900

    #5 이미지 페이지 구성

--- src/components/images/ImageGallery.js
+++ src/components/images/ImageGallery.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './ImageGallery.css';
 
 const ImageGallery = ({ images }) => {
-    console.log(images);
-    return <section></section>;
+    return (
+        <section className="image-gallery">
+            {images.length > 0 ? (
+                images.map((image) => <img key={image.id} src={image.urls.small} alt={image.alt_description} />)
+            ) : (
+                <></>
+            )}
+        </section>
+    );
 };
 
 export default ImageGallery;
