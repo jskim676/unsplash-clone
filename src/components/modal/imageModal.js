a39026e5013790a75e6e27e949744bdc76af45d4
jskim676 <wkxndlsvk@gmail.com>
2024-06-06 00:05:55 +0900

    #13 모달창 이미지 CSS

--- src/components/modal/imageModal.js
+++ src/components/modal/imageModal.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import './imageModal.css';
 import { statsPhoto } from '../unsplash-api';
 
 const ImageModal = ({ isOpen, closeModal, imgModalData }) => {
-    if (imgModalData.length > 0) {
-        console.log(imgModalData);
-        statsPhoto(imgModalData.id).then((result) => {
-            const photoViews = result.response;
-            console.log(photoViews);
-        });
-    }
+    const [photoViews, setPhotoViews] = useState(null);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+            setPhotoViews(null);
+        }
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (imgModalData && imgModalData.id) {
+            statsPhoto(imgModalData.id).then((result) => {
+                setPhotoViews(result.response);
+            });
+        }
+    }, [imgModalData]);
+
+    const numberComma = (number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    };
     return (
         <div>
-            {isOpen ? (
+            {isOpen && (
                 <Modal
                     isOpen={isOpen}
                     onRequestClose={closeModal}
@@ -21,18 +39,36 @@ const ImageModal = ({ isOpen, closeModal, imgModalData }) => {
                     overlayClassName="modal-overlay"
                 >
                     <div className="modal-header">
-                        <img src={imgModalData.user.profile_image.large} alt="" />
+                        <img src={imgModalData.user.profile_image.large} alt={`${imgModalData.user.name} profile`} />
                         <span>{imgModalData.user.name}</span>
                     </div>
                     <div className="modal-main">
-                        <img src={imgModalData.urls.regular}></img>
+                        <img src={imgModalData.urls.regular} alt="Modal content" />
+                    </div>
+                    <div className="modal-footer">
+                        {photoViews ? (
+                            <>
+                                <div className="img-view-info">
+                                    <span>조회수</span>
+                                    <span>{numberComma(photoViews.views.total)}</span>
+                                </div>
+                                <div className="img-view-info">
+                                    <span>다운로드</span>
+                                    <span>{numberComma(photoViews.downloads.total)}</span>
+                                </div>
+                                <div className="img-view-info">
+                                    <span>게시날짜</span>
+                                    <span>{imgModalData.created_at.slice(0, 10)}</span>
+                                </div>
+                            </>
+                        ) : (
+                            <></>
+                        )}
                     </div>
                     <button className="close-modal" onClick={closeModal}>
-                        Close
+                        <img src="/close-icon.svg" alt="Close modal" />
                     </button>
                 </Modal>
-            ) : (
-                <></>
             )}
         </div>
     );
